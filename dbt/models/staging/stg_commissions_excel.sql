f184f560f78dd87a5ca1cc220d122432b6f389aa
Alex Ino <cruceru_alex@hotmail.it>
2026-09-25 13:37:14 +0300

    Set up dbt project and initial staging model

---
+++ dbt/models/staging/stg_commissions_excel.sql
@@ -0,0 +1,21 @@
+SELECT
+    source_sheet,
+    source_row,
+    mese_raw,
+    slot_raw,
+    completo_raw,
+    priorita_raw,
+    cliente_raw,
+    ordine_raw,
+    stato_raw,
+    data_ordine_raw,
+    deadline_raw,
+    prezzo_raw,
+    tassa_paypal_raw,
+    entrata_raw,
+    contatto_raw,
+    online_handle_raw,
+    loaded_at,
+    note_raw
+
+FROM {{ source('commissions_raw', 'commissions_excel') }}
